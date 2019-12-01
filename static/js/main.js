d6618f77bac8b3cf1d9a7ab37364fda0eae7e105
DanielFrancaContaGit <daniel.pe.15y15@gmail.com>
2019-12-01 18:23:04 -0300

    footer

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,5 @@
 const espaco = document.querySelector('.div-quem_somos');
 const tresPilares = document.querySelector('.tres_pilares');
-const navegation = document.querySelector('.navigation');
 
 tresPilares.style.marginTop = `${ espaco.offsetHeight -130 }px`;
 
