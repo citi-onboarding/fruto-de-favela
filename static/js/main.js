88ff1a6af3a280e20e8c5e42ddbd1c7f070215d7
DanielFrancaContaGit <daniel.pe.15y15@gmail.com>
2019-12-02 10:29:07 -0300

    atualisação

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,9 @@ const espaco = document.querySelector('.div-quem_somos');
 const tresPilares = document.querySelector('.tres_pilares');
 
 
-tresPilares.style.marginTop = `${ espaco.offsetHeight -130 }px`;
+/*tresPilares.style.marginTop = `${ espaco.offsetHeight -130 }px`;*/
+
+tresPilares.style.marginTop = `${ espaco.offsetHeight -90 }px`;
 
 
 
