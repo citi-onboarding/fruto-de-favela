2d5a52daeef02722291f58f48089dd9c4b62bc6a
DanielFrancaContaGit <daniel.pe.15y15@gmail.com>
2019-12-03 16:04:43 -0300

    snick

--- static/js/main.js
+++ static/js/main.js
@@ -7,7 +7,33 @@ const tresPilares = document.querySelector('.tres_pilares');
 tresPilares.style.marginTop = `${ espaco.offsetHeight -90 }px`;
 
 
+/* slick teste */
 
+$('.carrocel').slick({
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    variableWidth: true
+});
 
+if (Event.pageX <= 930){
+   /* $('.slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.slider-nav'
+      });
+    $('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+    });*/
+    console.log('mobile');
+}
 
 
