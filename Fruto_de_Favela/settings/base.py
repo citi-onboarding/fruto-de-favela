8c7870424de74e75b9cdad8e0713c04406b092b5
argas7 <argas@cin.ufpe.br>
2019-12-04 16:45:32 -0300

    settings.py

--- Fruto_de_Favela/settings/base.py
+++ Fruto_de_Favela/settings/base.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Fruto_de_Favela.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'core/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,7 +107,7 @@ STATICFILES_DIRS = [
 ]
 
 #Media files
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media'),
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # Internationalization
